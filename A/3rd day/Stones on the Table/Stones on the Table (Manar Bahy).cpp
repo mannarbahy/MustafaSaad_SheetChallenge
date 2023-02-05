685a7ce4bb6c6cc06a4b83e8fd969b2d77ed8190
manarbahy171 <manarbahy69@gmail.com>
2023-02-06 00:28:27 +0200

    problem

---
+++ A/3rd day/Stones on the Table/Stones on the Table (Manar Bahy).cpp
@@ -0,0 +1,25 @@
+// author "Manar Bahy"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+ int main()
+{  
+          	ios_base::sync_with_stdio(false);   cin.tie(NULL);   cout.tie(NULL);
+        	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+ 
+  
+  	int n; cin>>n;
+   	string  s;	
+
+	ll  cnt= 0;		cin>>s;
+	for (int i = 0 ; i < n ; i++ ){
+
+		if (s[i]==s[i+1]) cnt++;
+	}
+	
+	cout<<cnt; 
+
+      return 0; 
+}
